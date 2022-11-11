7367a4b1677ef680c39d0267fa520e1a0a4ea931
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2022-11-11 11:38:06 -0300

    AluraTube 2

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
+import React from "react"
 import config from "../config.json"
 import styled from "styled-components"
 import { CSSReset } from "../src/components/CSSReset"
 import Menu from "../src/components/Menu"
 import { StyledTimeline } from "../src/components/Timeline"
 
-
-
 function HomePage() {
-    // const estilo = { backgroundColor: 'blueviolet', color: 'white' }
+   
+    const [filterValue, setFilterValue] = React.useState('')
+    
     return (
         <>
             <CSSReset />
             <div>
-                <Menu />
+                <Menu filterValue={filterValue} setFilterValue={setFilterValue}/>
+                <Banner />
                 <Header />
-                <Timeline playlists={config.playlists} />
+                <Timeline searchValue={filterValue} playlists={config.playlists} />
             </div>
         </>
     )
 }
 
 export default HomePage
-
 
 const StyledHeader = styled.div`
   
@@ -32,7 +33,6 @@ const StyledHeader = styled.div`
     }
 
     .user-info {
-        margin-top: 3.5rem;
         display: flex;
         align-items: center;
         width: 100%;
@@ -44,7 +44,6 @@ const StyledHeader = styled.div`
 function Header() {
     return (
         <StyledHeader>
-            {/* <img src="banner.png"/> */}
             <section className="user-info">
                 <img src={`https://github.com/${config.github}.png`} />
                 <div>
@@ -57,7 +56,28 @@ function Header() {
     )
 }
 
-function Timeline(props) {
+const StyledBanner = styled.div`
+.banner {
+    margin-top: 3.5rem;
+    height: 230px;
+    background-image: url(https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+`
+function Banner() {
+    return (
+        <StyledBanner>
+            <div className="banner">
+                
+            </div>
+        </StyledBanner>
+    )
+}
+
+
+function Timeline({searchValue, ...props}) {
 
     const playlistNames = Object.keys(props.playlists)
 
@@ -68,12 +88,16 @@ function Timeline(props) {
 
                 const videos = props.playlists[playlistName]
                 return (
-                    <section>
+                    <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
-                            {videos.map((video) => {
+                            {videos.filter((video) => {
+                                const titleNormalized = video.title.toLowerCase()
+                                const searchValueNormalized = searchValue.toLowerCase()
+                                return titleNormalized.includes(searchValueNormalized)
+                            }).map((video) => {
                                 return (
-                                    <a href={video.url}>
+                                    <a key={video.url} href={video.url}>
                                         <img src={video.thumb} />
                                         <span>{video.title}</span>
                                     </a>
